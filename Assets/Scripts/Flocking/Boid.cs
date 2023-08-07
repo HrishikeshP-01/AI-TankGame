464f9f13a2f286e9b291c1e893f0f78be9ba3fb5
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-07 22:59:16 +0530

    Flocking - Created Boid & Flocking scripts

---
+++ Assets/Scripts/Flocking/Boid.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    [SerializeField]
+    private FlockController flockController;
+    //Modified direction of the boid
+    private Vector3 targetDirection;
+    //Current direction of the boid
+    private Vector3 direction;
+
+    public FlockController FlockController
+    {
+        get { return FlockController; }
+        set { flockController = value; }
+    }
+
+    public Vector3 Direction { get { return direction; } }
+
+    private void Awake()
+    {
+        direction = transform.forward.normalized;
+        if(flockController==null)
+        {
+            Debug.LogError("Flock controller not initialized");
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        if (targetDirection == Vector3.zero) { return; }
+
+        direction = targetDirection.normalized;
+        direction *= FlockController.speedModifier;
+        transform.Translate(direction * Time.deltaTime);
+    }
+}
