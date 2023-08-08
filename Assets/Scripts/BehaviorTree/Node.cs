738db53fe8fbd0dd18e992d05352aa16d408ef37
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-08 19:35:11 +0530

    Behavior tree - Implemented Node abstract class

---
+++ Assets/Scripts/BehaviorTree/Node.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class Node
+{
+    //Delegate that returns the state of the node
+    public delegate NodeStates NodeReturn();
+
+    //Current state of the node
+    protected NodeStates nodeState;
+
+    // Getter fn for value of nodeState
+    public NodeStates NodeState { get { return nodeState; } }
+
+    //Constructor for Node
+    Node() { }
+
+    //Implementing classes use this method to evaluate the desired set of conditions
+    public abstract NodeStates Evaluate();
+}
