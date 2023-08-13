2f60ddcd98bdbd2d5f32b4552d1fe83ab5037955
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-08-13 10:52:39 +0530

    Created explosion, animation & explosion state

---
+++ Assets/Scripts/TowerDefense/ExplosionAnimation/ExplosionState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionState : StateMachineBehaviour
+{
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Destroy(animator.gameObject, 0.1f);
+    }
+}
